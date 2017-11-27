6270a1c3aa39f79b83a125ea96951c57a2f4c172
Brian Schroer <brianschroer@gmail.com>
2017-11-27 13:30:49 -0600

    Added ActivityIndicator

--- components/WebViewComponent.js
+++ components/WebViewComponent.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { WebView } from 'react-native';
+import { View, WebView, ActivityIndicator } from 'react-native';
 
 export default class WebViewComponent extends React.Component {
   static navigationOptions = {
     title: 'Web View'
   };
 
-  state = {};
+  state = {
+    isLoading: true
+  };
+
+  handleWebViewLoaded = () => this.setState({ isLoading: false });
 
   render() {
+    const { isLoading } = this.state;
     return (
-      <WebView
-        source={{
-          uri: 'https://github.com/BrianSchroer/learning-react-native'
-        }}
-        style={{ marginTop: 20 }}
-      />
+      <View style={{ flex: 1 }}>
+        {isLoading && <ActivityIndicator />}
+        <WebView
+          source={{
+            uri: 'https://github.com/BrianSchroer/learning-react-native'
+          }}
+          style={{ flex: 1 }}
+          onLoad={this.handleWebViewLoaded}
+        />
+      </View>
     );
   }
 }
